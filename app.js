72c1d0475414a62bff75952056f5132ee1d47bca
RCH <rchillard@gmail.com>
2019-07-13 23:14:42 -0500

    Initialize as node package, add EJS template, rankings table

--- app.js
+++ app.js
@@ -1,26 +1,43 @@
 // SETUP
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
-var ejs = require("ejs");
+const express = require("express");
+const bodyParser = require("body-parser");
+
+// CONFIG
+const app = express();
+app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.set("view engine", ejs);
+
+// DATA
+let avengers = [
+  { name: "Iron Man", votes: 0 },
+  { name: "Thor", votes: 0 },
+  { name: "Black Widow", votes: 0 },
+  { name: "Hulk", votes: 0 },
+  { name: "Captain America", votes: 0 },
+  { name: "Black Panther", votes: 0 }
+];
 
 // ROUTES
 app.get("/", function(request, response) {
   console.log("Root route accessed");
-  response.render("index");
+  response.render("index", { avengers: avengers });
 });
 
 app.post("/vote", function(request, response) {
-  console.log("Received a POST to /vote");
-  console.log(request.body.avengervote);
-  response.send("RECEIVED VOTE!");
+  console.log("Received a POST to /vote for: " + request.body.avengervote);
+  avengers.find(function(avenger, index) {
+    if (avenger.name === request.body.avengervote) {
+      avenger.votes++;
+    }
+  });
+  console.log(avengers);
+  response.redirect("/");
 });
 
-// SERVER RUN
+// START SERVER
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, function() {
   console.log("Avenger-vote Server has started on port " + PORT);
 });
